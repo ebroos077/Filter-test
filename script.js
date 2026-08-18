290ef45dec71b16b0932f0de8d7a00c03c8d2895
ebroos077 <ebrahimi.alexander@gmail.com>
2026-08-18 10:36:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const stateMap = {
 };
 
 const houses = [
-  {proptype:"Single-Family Home",       city:"Las Vegas, NV",      loanamt:420000,   loan:"Conventional", rate:6.75, refi:false, type:"Rehab Bridge", term:30, link:"https://bluecactuslending.com/casestudy-1"},
+  {proptype:"Single-Family Home",       city:"Las Vegas, NV",      loanamt:420000,   loan:"Bridge", rate:6.75, refi:false, type:"Brokered Loan", term:30, link:"https://bluecactuslending.com/casestudy-1"},
   {proptype:"Condo",                    city:"Henderson, NV",      loanamt:310000,   loan:"FHA",           rate:6.25, refi:false, type:"Funded Loan",   term:30, link:"https://bluecactuslending.com/casestudy-2"},
   {proptype:"Townhouse",                city:"Reno, NV",           loanamt:550000,   loan:"Conventional", rate:7.00, refi:true,  type:"Brokered Loan", term:15, link:"https://bluecactuslending.com/casestudy-3"},
   {proptype:"Single-Family Home",       city:"Los Angeles, CA",    loanamt:890000,   loan:"Conventional", rate:6.90, refi:true,  type:"Funded Loan",   term:30, link:"https://bluecactuslending.com/casestudy-4"},
@@ -39,7 +39,7 @@ const houses = [
   {proptype:"Single-Family Home",       city:"Reno, NV",           loanamt:320000,   loan:"Conventional", rate:6.55, refi:true,  type:"Brokered Loan", term:15, link:"https://bluecactuslending.com/casestudy-25"},
   {proptype:"Manufactured Home",        city:"Tucson, AZ",         loanamt:260000,   loan:"FHA",           rate:6.15, refi:false, type:"Funded Loan",   term:30, link:"https://bluecactuslending.com/casestudy-26"},
   {proptype:"Single-Family Home",       city:"Sacramento, CA",     loanamt:590000,   loan:"Conventional", rate:6.90, refi:true,  type:"Brokered Loan", term:30, link:"https://bluecactuslending.com/casestudy-27"},
-  {proptype:"Single-Family Home",       city:"Denver, CO",         loanamt:675000,   loan:"Construction",  rate:8.25, refi:false, type:"Funded Loan",   term:25, link:"https://bluecactuslending.com/casestudy-29"},
+  {proptype:"Single-Family Home",       city:"Denver, CO",         loanamt:675000,   loan:"Construction",  rate:8.25, refi:false, type:"",   term:25, link:"https://bluecactuslending.com/casestudy-29"},
   {proptype:"Single-Family Home",       city:"Las Vegas, NV",      loanamt:445000,   loan:"Construction",  rate:8.50, refi:false, type:"Brokered Loan", term:25, link:"https://bluecactuslending.com/casestudy-30"},
   {proptype:"Single-Family Home",       city:"Nashville, TN",      loanamt:520000,   loan:"Conventional", rate:6.85, refi:true,  type:"Funded Loan",   term:30, link:"https://bluecactuslending.com/casestudy-31"},
   {proptype:"Townhouse",                city:"Phoenix, AZ",        loanamt:388000,   loan:"FHA",           rate:6.35, refi:false, type:"Brokered Loan", term:30, link:"https://bluecactuslending.com/casestudy-32"},
